f16c13a41d4aba64fe2cebb0a817ba4409783ad3
amcassano <54558244+amcassano@users.noreply.github.com>
2023-09-18 17:30:48 -0500

    added gsea funcitons

---
+++ R/getGSEA.R
@@ -0,0 +1,32 @@
+#' Get GSEA
+#'
+#' @param degs annotated, pairwise comparison dataframe, *not* the significant genes
+#' @param ontol string, either "BP", "MF", "CC", or "ALL", defaults to ALL
+#'
+#' @return GSEA object
+#' @export
+#'
+#' @examples
+#' getGSEAobj(tol_vs_naive, ontol = "BP")
+getGSEAobj <- function(degs, ontol = "ALL"){
+  #set up the gene set - get fold change and gene names
+  geneset <- degs$Log2FoldChange
+  tibble::rownames_to_column(degs, var = "GeneID")
+  names(geneset) <- degs$GeneID
+  geneset <- stats::na.omit(geneset)
+  geneset <- sort(geneset, decreasing = TRUE)
+
+  #call gene set function
+  gsea <- clusterProfiler::gseGO(geneList = geneset,
+                      ont = ontol,
+                      keyType = "ENSEMBL",
+                      minGSSize = 1,
+                      maxGSSize = 800,
+                      pvalueCutoff = 0.05,
+                      verbose = F,
+                      OrgDb = "org.Mm.eg.db",
+                      pAdjustMethod = "fdr")
+
+  return(gsea)
+}
+
