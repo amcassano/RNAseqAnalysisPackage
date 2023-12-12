26154674898ecdfafa70603cd21747810dc97413
amcassano <54558244+amcassano@users.noreply.github.com>
2023-12-12 16:38:21 -0600

    Update categorynetplot.R

--- R/categorynetplot.R
+++ R/categorynetplot.R
@@ -4,7 +4,6 @@
 #' @param deg_df dataframe
 #' @param pval_cutoff number
 #' @param l2fc_cutoff number
-#' @param ontol string; either "ALL", "BP", "MF", or "CC"
 #' @param categories_to_show number; defaults to 30
 #' @param seed number; defaults to 2, for set seed for reproducibility
 #' @param shape string; one of "star", "circle", "gem", "dh", 'graphopt', 'grid', 'mds', 'randomly', 'fr', 'kk', 'drl' or 'lgl'.
@@ -14,7 +13,7 @@
 #'
 #' @examples
 #' categorynetplot(goobj, one_vs_two)
-categorynetplot <- function(go_obj, deg_df, pval_cutoff = 0.05, l2fc_cutoff = 0.5, categories_to_show = 30, ontol = "ALL", seed = 2, shape = "circle"){
+categorynetplot <- function(go_obj, deg_df, pval_cutoff = 0.05, l2fc_cutoff = 0.5, categories_to_show = 30, seed = 2, shape = "circle"){
   #remove any rows with no Gene ID or with duplicates (there shouldn't be any but it'd mess everything up)
   deg_df <- tibble::rownames_to_column(deg_df, var = "GeneID")
   deg_df <- dplyr::filter(deg_df, !is.na(GeneID))
