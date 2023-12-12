1a6f9221ae82d8654d7685e0343881fdd2d09ce6
amcassano <54558244+amcassano@users.noreply.github.com>
2023-12-12 16:09:45 -0600

    Update categorynetplot.R

--- R/categorynetplot.R
+++ R/categorynetplot.R
@@ -6,13 +6,14 @@
 #' @param l2fc_cutoff number
 #' @param ontol string; either "ALL", "BP", "MF", or "CC"
 #' @param categories_to_show number; defaults to 30
+#' @param seed number; defaults to 2, for set seed for reproducibility
 #'
 #' @return category netplot
 #' @export
 #'
 #' @examples
 #' categorynetplot(goobj, one_vs_two)
-categorynetplot <- function(go_obj, deg_df, pval_cutoff = 0.05, l2fc_cutoff = 0.5, categories_to_show = 30, ontol = "ALL"){
+categorynetplot <- function(go_obj, deg_df, pval_cutoff = 0.05, l2fc_cutoff = 0.5, categories_to_show = 30, ontol = "ALL", seed = 2){
   #remove any rows with no Gene ID or with duplicates (there shouldn't be any but it'd mess everything up)
   deg_df <- tibble::rownames_to_column(deg_df, var = "GeneID")
   deg_df <- dplyr::filter(deg_df, !is.na(GeneID))
@@ -25,7 +26,7 @@ categorynetplot <- function(go_obj, deg_df, pval_cutoff = 0.05, l2fc_cutoff = 0.
 
   sigGenesFC <- sigDEGs$Log2FoldChange
   names(sigGenesFC) <- sigDEGs$GeneID
-
+  set.seed(seed)
   cnet <-
     enrichplot::cnetplot(go_obj, showCategory = categories_to_show, foldChange = sigGenesFC)
 
