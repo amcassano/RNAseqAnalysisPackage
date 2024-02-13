47f40e15075c571dd49d2daf5c824e21ff1335f3
amcassano <54558244+amcassano@users.noreply.github.com>
2024-02-13 14:08:37 -0600

    updated comp volcano

--- R/comparativeVolcano.R
+++ R/comparativeVolcano.R
@@ -3,9 +3,8 @@
 #' @param deg_df1 dataframe of pairwise comparison results, must be annotated
 #' @param deg_df2 dataframe of pairwise comparison results, must be annotated
 #' @param plotTitle string, plot title
-#' @param cond1 string, the name of the numerator condition in deg_df1
-#' @param cond2 string, the name of the numerator condition in deg_df2
-#' @param denom string, the name of the denominator condition
+#' @param cond1 string, the name of the comparison in deg_df1 (ideally as "X vs. Y")
+#' @param cond2 string, the name of the comparison in deg_df2 (ideally as "X vs. Y")
 #' @param l2fc_cutoff number, where to draw the cutoff lines for log2 fold changes and to determine which points to color/label
 #' @param pval_cutoff number, points with P value lower than this in either dataframe will not be plotted
 #' @param dircolors list of 5 colors (strings) for both up, both down, up in 1 & down in 2, up in 2 & down in 1, and not changed
@@ -16,7 +15,7 @@
 #' @export
 #'
 #' @examples comp_volcano(Rej_vs_Tol, Naive_vs_Tol, "Naive and Rejecting comparison", "Rejecting", "Naive", "tolerant")
-comp_volcano <- function (deg_df1, deg_df2, plotTitle, cond1, cond2, denom, l2fc_cutoff = 0.8, pval_cutoff = 0.45,
+comp_volcano <- function (deg_df1, deg_df2, plotTitle, cond1, cond2, l2fc_cutoff = 0.8, pval_cutoff = 0.45,
                           dircolors = c("#a50000", "#00009c", "darkgreen", "purple4", "gray70"),
                           dirshapes = c(24, 25, 23, 23, 21), overlaps = 50)
 {
@@ -43,11 +42,11 @@ comp_volcano <- function (deg_df1, deg_df2, plotTitle, cond1, cond2, denom, l2fc
   deg_df2 <- dplyr::filter(deg_df2, MGI_Symbol != "", !dupe, !is.na(MGI_Symbol))
 
   #set labels
-  bothUp <- paste("Up in both", cond1, "and", cond2, "compared to", denom, sep = " ")
-  oneUpOnly <- paste("Up in", cond1, "and down in", cond2,"compared to", denom, sep = " ")
-  twoUpOnly <- paste("Up in", cond2, "and down in", cond1, "compared to", denom, sep = " ")
-  bothDown <- paste("Down in both",cond1, "and", cond2, "compared to", denom, sep = " ")
-  notsig <- paste("Not up or down", "in either", cond1, "or", cond2, "compared to", denom, sep = " ")
+  bothUp <- paste("Up in both", cond1, "and", cond2, sep = " ")
+  oneUpOnly <- paste("Up in", cond1, "and down in", cond2, sep = " ")
+  twoUpOnly <- paste("Up in", cond2, "and down in", cond1, sep = " ")
+  bothDown <- paste("Down in both",cond1, "and", cond2, sep = " ")
+  notsig <- paste("Not up or down", "in either", cond1, "or", cond2, sep = " ")
   names(dircolors) <- c(bothUp, bothDown, oneUpOnly, twoUpOnly, notsig)
   names(dirshapes) <- c(bothUp, bothDown, oneUpOnly, twoUpOnly, notsig)
   legorder <- c(bothUp, bothDown, oneUpOnly, twoUpOnly, notsig)
@@ -81,13 +80,13 @@ comp_volcano <- function (deg_df1, deg_df2, plotTitle, cond1, cond2, denom, l2fc
     ggplot2::scale_x_continuous(expand = ggplot2::expansion(mult = 0.25)) +
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = 0.15)) +
     ggplot2::labs(title = plotTitle,
-                  subtitle = paste(cond1, "/", denom, " DEGs vs. ", cond2, "/", denom, " DEGs", sep = ""),
+                  subtitle = paste(cond1, " DEGs vs. ", cond2," DEGs", sep = ""),
                   caption = paste("\n Log2 FC cutoff: ", l2fc_cutoff,
                                   "\n", "\n",
                                   "Not showing any points with adj. P value of greater than ", pval_cutoff,
                                   sep = ""),
-                  x = paste("Log2 Fold Change ", cond1, " vs. ", denom, sep = ""),
-                  y = paste("Log2 Fold Change ", cond2, " vs. ", denom, sep = "")) +
+                  x = paste("Log2 Fold Change ", cond1, sep = ""),
+                  y = paste("Log2 Fold Change ", cond2, sep = "")) +
     ggplot2::geom_vline(xintercept = c(-1 * l2fc_cutoff,l2fc_cutoff), col = "gray85") +
     ggplot2::geom_hline(yintercept = c(-1 * l2fc_cutoff,l2fc_cutoff), col = "gray85") +
     ggrepel::geom_text_repel(min.segment.length = 0,
