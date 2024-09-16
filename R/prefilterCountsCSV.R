9b03e10d7ea39657df7214f5c1f803145859ed26
amcassano <54558244+amcassano@users.noreply.github.com>
2024-09-16 16:29:30 -0500

    Update prefilterCountsCSV.R

--- R/prefilterCountsCSV.R
+++ R/prefilterCountsCSV.R
@@ -4,6 +4,7 @@
 #' @param rawcounts raw counts dataframe
 #' @param numberofsamples number, the total number of samples in the counts dataframe
 #' @param avg_count_minimum number, defaults to 200, higher number will be more stringent filtering, lower number will be more generous
+#' @param stringency number, the denominator for the second filtering. defaults to 2 which means that genes where the top half of the counts meet the avg will be kept. if 3 then only 1/3 of counts need to meet it etc
 #'
 #' @return counts dataframe
 #' @export
@@ -11,11 +12,26 @@
 #' @examples
 #' prefilterCounts(raw_counts, 17)
 #' prefilterCounts(raw_counts, 13, 150)
-prefilterCounts <- function(rawcounts, numberofsamples, avg_count_minimum = 200){
-  row_min <- avg_count_minimum * numberofsamples
-  rawcounts$row_max <- apply(rawcounts, 1, max)
-  rawcounts$row_sum <- apply(rawcounts, 1, sum)
-  rawcounts <- dplyr::filter(rawcounts, row_sum - row_max * 2 > row_min)
-  rawcounts <- dplyr::select(rawcounts, -c(row_sum, row_max))
+prefilterCounts <- function(rawcounts, numberofsamples, avg_count_minimum = 200, stringency = 2){
+
+  frac_samples <- ceiling(numberofsamples / stringency)
+  row_min <- avg_count_minimum * (numberofsamples - 1)
+  row_min_frac <- avg_count_minimum * frac_samples
+  rawcounts <- tibble::column_to_rownames(rawcounts, var = "Geneid")
+  rawcounts <- dplyr::select(rawcounts, -Geneid)
+  rawcounts$rowmax <- apply(rawcounts, 1, max)
+  rawcounts$rowsum <- apply(rawcounts, 1, sum)
+  rawcounts <- tibble::rownames_to_column(raw_counts, var = "rowid")
+  rawcounts <- rawcounts %>% dplyr::rowwise %>%
+    dplyr::mutate(topfrac = sum(sort(dplyr::c_across(
+      tidyselect::starts_with("row")
+    ), decreasing = TRUE)[1:frac_samples])) %>%
+    dplyr::ungroup()
+  rawcounts <- dplyr::filter(rawcounts,
+                              rowsum - rowmax * 2 >= row_min |
+                                topfrac >= row_min_frac)
+  rawcounts <- dplyr::select(raw_counts, -c(rowsum, rowmax))
+  rawcounts <- dplyr::column_to_rownames(rawcounts, var = "rowid")
+  rawcounts <- dplyr::select(rawcounts, -rowid)
   return(rawcounts)
 }
